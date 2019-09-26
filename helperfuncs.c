7d6f32c1880e845cc97814b2c39008c6995119c8
Faizan Khan <mathemagician229@gmail.com>
2019-09-26 17:44:10 +0000

    Adding first helper function.

---
+++ helperfuncs.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+/**
+ * _strcmp - compares strings
+ * @s1: a string to be compared
+ * @s2: a string to be compared
+ * Return: 0
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0')
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else
+			break;
+	}
+	return (0);
+}
